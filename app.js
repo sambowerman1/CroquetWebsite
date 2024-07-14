e0f2b11f9d544400a494f5094a63ae2c2a0f6dcb
Sam Bowerman <sam@bowerman.org>
2024-07-14 01:55:14 -0500

    added port

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname)); // Serve static files from current directory
 
