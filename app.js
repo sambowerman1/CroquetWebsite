d616fd55324e50d4218926b2b6311667dcd0ce1b
Sam Bowerman <sam@bowerman.org>
2024-07-14 01:30:00 -0500

    *

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname)); // Serve static files from current directory
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html')); // Serve your index.html file
+});
+
+app.get('/players', (req, res) => {
+    res.sendFile(path.join(__dirname, 'players.html')); // Serve your players.html file
+});
+
+app.get('/standings', (req, res) => {
+    res.sendFile(path.join(__dirname, 'standings.html')); // Serve your standings.html file
+});
+
+app.listen(3000, () => console.log('Server running on port 3000')); // Listen on port 3000
